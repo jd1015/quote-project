b846c88792273213206c0f466d0da9ed00276ce4
daodai xiangtai <daodaixiangtai73@gmail.com>
2017-11-01 21:10:17 +0900

    Create an empty file

---
+++ src/test/java/com/example/demo/service/QuoteServiceImplTest.java
@@ -0,0 +1,53 @@
+/**
+ * 
+ */
+package com.example.demo.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author lepra
+ *
+ */
+public class QuoteServiceImplTest {
+
+  /**
+   * @throws java.lang.Exception
+   */
+  @BeforeClass
+  public static void setUpBeforeClass() throws Exception {
+  }
+
+  /**
+   * @throws java.lang.Exception
+   */
+  @AfterClass
+  public static void tearDownAfterClass() throws Exception {
+  }
+
+  /**
+   * @throws java.lang.Exception
+   */
+  @Before
+  public void setUp() throws Exception {
+  }
+
+  /**
+   * @throws java.lang.Exception
+   */
+  @After
+  public void tearDown() throws Exception {
+  }
+
+  @Test
+  public void test() {
+    fail("まだ実装されていません"); // TODO
+  }
+
+}
